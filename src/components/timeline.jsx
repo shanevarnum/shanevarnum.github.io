9b4707478dd2189e507845d2e85cf89b031a6a60
Shane <shanevarnum@outlook.com>
2021-03-31 02:46:05 -0400

    Copy over all the files to host for Github pages

---
+++ src/components/timeline.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Technology Analyst <span>2019-Present</span></h2>
+                        <p>I have been a part of the rotational tech analyst program at Citi working in primarily Production Support with some devops and automation thrown into the mix. I have worked with Java, Linux, SQL, uDeploy, and Docker in this role. I am looking to break into the development space with the added benefit of my production environment experience and business knowledge</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Assurance Support Analyst<span>Jan 2019-Jul 2019</span></h2>
+                        <p>Provided application and end user support for Aura applications</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>B.S. in Information Systems and Business Analytics<span>2013-2018</span></h2>
+                        <p>I have completed my bachelors of science degree at the University of South Florida. I have taken courses related to: OOP, Computer Networks, Data Modeling and Business Intelligence, and Advanced Database Administration</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
